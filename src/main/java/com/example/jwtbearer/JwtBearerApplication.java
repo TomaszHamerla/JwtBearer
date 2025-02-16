3cbd449629e9d119ea69edd68670b31ac9d57a32
unknown <tomaszhamerla@interia.pl>
2025-02-16 18:43:44 +0100

    init

---
+++ src/main/java/com/example/jwtbearer/JwtBearerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtbearer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBearerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtBearerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtbearer/JwtBearerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtbearer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBearerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
